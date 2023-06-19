3d0537de937990bb6f6386a3490751bdf0af9175
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2023-06-19 22:49:08 +0700

    fix css or style

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,9 @@ export default {
       selectTweet: false
     }
   },
+  mounted() {
+    this.$refs.embeddedTweet.focus()
+  },
   computed: {
     // adalah salinan dan tweet: true atau false
     isCopy: function() {
@@ -31,14 +34,20 @@ export default {
   watch: {
     // textarea: embeddedTweet
     embeddedTweet() {
-    // vue methods: memuat
+      // vue methods: memuat
       this.carry()
     }
   },
   methods: {
     // memuat: dari textarea embeddedTweet ini
-    carry() {
+    async carry() {
       let newEmbeddedTweet = ''
+
+      if (this.embeddedTweet == '') {
+        this.embeddedTweet = ''
+        this.isEmbeddedTweetDefault()
+        return
+      }
 
       // regex101.com
       const regex = /https:\/\/twitter\.com\/(\w.*)\/status\/(\d*)|\?\w.*/gm
@@ -56,7 +65,6 @@ export default {
         
         // The result can be accessed through the `m`-variable.
         m.forEach((match, groupIndex) => {
-          
           if (match !== undefined && groupIndex === 1) {
             profile = match
           }
@@ -66,6 +74,9 @@ export default {
           }
         })
       }
+
+      console.log('profile:', profile);
+      console.log('status:', status);
 
       if (profile != '' && status != '') {
         newEmbeddedTweet = `https://twitter.com/${profile}/status/${status}/video/1`
@@ -85,8 +96,15 @@ export default {
       
       this.embeddedTweet = newEmbeddedTweet
     },
+
+    // button: reset, copy dan tweet
+    btnReset() {
+      this.embeddedTweet == ''
+      this.$refs.embeddedTweet.focus()
+      this.selectCopy = false
+      this.selectTweet = false
+    },
     
-    // button: copy dan tweet
     btnCopy() {
       if (this.embeddedTweet == '' || this.embeddedTweet == 'Tidak ada hasil') {
         return
@@ -109,8 +127,26 @@ export default {
     },
 
     btnCopyExample() {
-      navigator.clipboard.writeText('https://twitter.com/ockibagusp/status/1592924571732414465?s=20&t=bgO6hwTfDckbtQibxDJZPQ')
+      navigator.clipboard.writeText('Fedora 37! 👏\n\nhttps://twitter.com/ockibagusp/status/1592924571732414465?s=20&t=bgO6hwTfDckbtQibxDJZPQ')
     },
+
+    isNotEmbeddedTweet() {
+      this.embeddedTweet = ''
+      this.isEmbeddedTweetDefault()
+    },
+    isEmbeddedTweetDefault() {
+      // this.embeddedTweetBool = false
+      this.count = 280
+      this.selectCopy = false
+      this.selectTweet = false
+    },
+    isEmbeddedTweetSuccess(videoLength) {
+      // this.embeddedTweetBool = true
+      this.selectResults = true
+      this.selectCopy = true
+      this.selectTweet = true
+      this.count = 280 - videoLength
+    }
   }
 }
 </script>
@@ -119,14 +155,20 @@ export default {
   <main>
     <h2 style="margin-top: 5px;">Menyematkan (embed) Video Tweet Tanpa Me-Retweet di Twitter</h2>
     <p style="margin-top: -15px;">Link: <a href="https://www.howtogeek.com/668753/how-to-embed-someones-twitter-video-without-retweeting-them" target="_blank">How to Embed Someone’s Twitter Video Without Retweeting Them!</a></p>
-    <textarea v-model="embeddedTweet" placeholder="https://twitter.com/ockibagusp/status/1592924571732414465?s=20&t=bgO6hwTfDckbtQibxDJZPQ" cols="50" rows="3" ref="embeddedTweet" style="margin-top: -10px;"></textarea>
-    <br>
-    <button @click="btnCopy" :disabled="isCopy">Copy</button>
-    <button @click="btnTweet" :disabled="isTweet">Tweet is: <small v-if="embeddedTweet.length < 280">+</small> {{count}}</button>
-    <br>
-    <p style="color: green">*Bagikan Video Twitter di Android dan Video Twitter Dari Web</p>
-    <p>Contoh: https://twitter.com/ockibagusp/status/1592924571732414465?s=20&t=bgO6hwTfDckbtQibxDJZPQ <button @click="btnCopyExample">Copy</button></p>
+    <textarea v-model="embeddedTweet" style="margin-top: -10px; width: 500px;height: 90px;" 
+      placeholder="Fedora 37! 👏 (2)
 
+https://twitter.com/ockibagusp/status/1592924571732414465?s=20&t=bgO6hwTfDckbtQibxDJZPQ" cols="50" rows="4" ref="embeddedTweet" data-test="embedded-tweet"></textarea>
+    <br>
+    <button @click="btnReset" data-test="btn-reset">Reset</button>
+    <button @click="btnCopy" data-test="btn-copy" :disabled="isCopy">Copy</button>
+    <button @click="btnTweet" data-test="btn-tweet" :disabled="isTweet">Tweet is: <small v-if="embeddedTweet.length < 280">+</small> {{count}}</button>
+    <strong style="margin-left: 10px; color: red;">*Tweet ini tidak ada hasil</strong>
+    <br>
+    <p style="color: green; margin-top: 8px; margin-bottom: 8px;">*Bagikan Video Twitter di Android dan Video Twitter Dari Web</p>
+    <p style="margin-bottom: 5px;">Contoh:</p>
+    <p style="margin-top: 5px;">Fedora 37! 👏 (2)</p>  
+    <p>https://twitter.com/ockibagusp/status/1592924571732414465?s=20&t=bgO6hwTfDckbtQibxDJZPQ <button @click="btnCopyExample">Copy</button></p>
   </main>
   </template>
 
